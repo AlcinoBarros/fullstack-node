b68a455ba90969dede10e1d783171a2f2c4c9a2c
comp1 <comp1@lab.unesc.net>
2019-07-19 20:37:05 -0300

    criou o model e o controller de usuario e autenticacao de login

---
+++ app/models/usuario.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const usuarioSchema = new Schema(
+    {
+        login: String,
+        senha: String,
+        email: String
+    },
+    { timestamps: true}
+)
+
+
+const usuario = mongoose.model("usuarios", usuarioSchema);
+
+module.exports = usuario;
--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,8 @@ const routes = express.Router();
 const ClienteController = require("../app/controllers/cliente-controller");
 const ItemController = require("../app/controllers/item-controller");
 const OrdemServicoController = require("../app/controllers/ordemServico-controller");
+const UsuarioController = require("../app/controllers/usuario-controller");
+const LoginController = require("../app/controllers/login-controller");
 
 routes.get("/clientes", ClienteController.findAll);
 routes.get("/clientes/:id", ClienteController.findById);
@@ -22,5 +24,13 @@ routes.get("/ordemservicos/:id", OrdemServicoController.findById);
 routes.post("/ordemservicos", OrdemServicoController.store);
 routes.put("/ordemservicos/:id", OrdemServicoController.update);
 routes.delete("/ordemservicos/:id", OrdemServicoController.delete);
+
+routes.get("/usuarios", UsuarioController.findAll);
+routes.post("/usuarios", UsuarioController.store);
+routes.post("/usuarios/validar", UsuarioController.usuarioValido);
+routes.put("/usuarios/:id", UsuarioController.update);
+routes.delete("/usuarios/:id", UsuarioController.delete);
+
+routes.post("/login", LoginController.fazerLogin);
 
 module.exports = routes;
